d2920747c7640c0fd338d0f78340cfbb558aafe0
Angel Balcarcel <angelcabo3@gmail.com>
2014-03-05 19:57:52 -0500

    Adding Rakefile for import task and a few admin views to quickly edit places without a location.

--- api.rb
+++ api.rb
@@ -3,22 +3,30 @@ require 'json'
 require_relative 'models/place'
 
 get '/' do
-  @places = Place.where(:lat.exists => false)
   erb :index
 end
 
-get '/place/:id/edit' do
-  @place = Place.find(params[:id])
-  erb :edit
+get '/admin/places' do
+  @query_string = request.query_string
+  if (params[:with_location] == 'true')
+    @places = Place.where(:lat.ne => "", :lat.exists => true)
+  elsif (params[:with_location] == 'false')
+    # @places = Place.where(:lat.ne => "", :lat.exists => false)
+    @places = Place.any_of({ lat: "" }, { :lat.exists => false })
+  else
+    @places = Place.all
+  end
+  erb :admin_index
 end
 
-patch '/place/:id/update' do
+get '/admin/place/:id/edit' do
+  @place = Place.find(params[:id])
+  @query_string = request.query_string
+  erb :admin_edit
+end
+
+patch '/admin/place/:id/update' do
   @place = Place.find(params[:id])
   @place.update_attributes!(formatted_address: params[:place][:formatted_address], lat: params[:place][:lat], lng: params[:place][:lng], website: params[:place][:website], phone: params[:place][:phone])
-  redirect to('/')
-end
-
-get '/foo.json' do
-  content_type :json
-  return {:foo => "bar"}.to_json
+  redirect to("/admin/places?#{request.query_string}")
 end
